0fd8dbc7c8f3d1aeb9b63de6d3344673ea3db3e6
Prajwal <prajwal9625@gmail.com>
2026-02-04 16:53:19 +0530

    Add files via upload

---
+++ python_chapter7 and practiceset/introduction_loop.py
@@ -0,0 +1,29 @@
+'''
+what is loops :- A loop is a programming structure that repeats a set of instruction until it becomes a TRUE, in python while loop 
+allows you to repeatedly execute a block of code as condition is TRUE
+
+there Are two types of loops:-
+1)while
+2)for 
+
+1)while:-this loop allows you to repeated a block of code as condition become true .
+
+syntax:- 
+while conditions:
+    
+'''
+conditions=True
+
+while conditions:
+    print("IM AN BEST ")                     #it runs and repeated again and again 
+    print("TRY AGAIN ")  #it runs and repeated again and again 
+    break                # so i used break to stop it cring or anything
+
+
+he_is_boy=True
+i=1
+while he_is_boy:
+    print("try[i]")
+    i=i+1
+     
+
