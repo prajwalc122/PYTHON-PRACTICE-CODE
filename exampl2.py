16d37d6be3f5b9db31bd65013321d11ad4c89bbf
Prajwal <prajwal9625@gmail.com>
2026-03-14 20:44:15 +0530

    Add files via upload

---
+++ exampl2.py
@@ -0,0 +1,10 @@
+#list can store yalla type data types 
+frends=["apple",34,44.33,False,"prajwal"]
+print(frends[0],frends[3],frends[4])
+#changing the third index values because list are mutable broo!!!!
+frends[3]="ninja"
+#index 3 alli iro False change agi ninja barutte aste 
+print(frends[0],frends[3],frends[4])
+
+# we try the string slicings
+print(frends[0:3])
